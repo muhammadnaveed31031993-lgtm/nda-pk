c65f8a34996cc3f74ef9ded14c42f918f493465f
muhammadnaveed31031993-lgtm <muhammadnaveed31031993@gmail.com>
2026-09-24 11:04:46 +0400

    Update supabaseClient.js

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
-const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY
+const supabaseUrl = 'https://qzyvoiugvmytiozxntft.supabase.co'
+const supabaseAnonKey = 'sb_publishable_7Mbn7QdW0dSb6gpEOM9Eww_L2si0vGS'
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
